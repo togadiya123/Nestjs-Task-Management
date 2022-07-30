3cff95c9e283c56ead7ec12a8144f79f4a4d60b8
Hardik Togadiya <hardiktogadiya123@gmail.com>
2022-07-31 00:14:15 +0530

    Try to connect a database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+console.log(process.env.HOST);
+console.log(process.env.PORT);
+console.log(process.env.USER);
+console.log(process.env.PASSWORD);
+console.log(process.env.DATABASE);
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: parseInt(process.env.PORT),
+      username: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+  ],
 })
 export class AppModule {}
